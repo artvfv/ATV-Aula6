546f0799328867fb44ac7774ccf769e89dfa65e1
Arthur Vinicius de Freitas Videres <arthur.vvideres@gmail.com>
2026-09-14 22:09:52 -0300

    Delete exercicio4.php

--- exercicio4.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$valor_original = 100;
-$possui_desconto = true;
-$valor_final = 0;
-
-echo $valor_original >= 150 || $possui_desconto ? "Valor final com DESCONTO: R$ " . ($valor_final = $valor_original * 0.9) : "Valor final: R$ " . $valor_original;
-
-?>
